6fae5cb90aea7d33d96c5523d8022f948b5a1f32
Nelson Nguyen <110303502+nelsonnguyen9878@users.noreply.github.com>
2023-09-28 12:43:48 -0700

    Update Textbook.cpp
    - added class header for information on the program
    - added comments

--- Textbook.cpp
+++ Textbook.cpp
@@ -1,14 +1,37 @@
-//
-// Created by Nelso on 9/26/2023.
-//
+/***************************************************************
+* wk1_reviewProgram.cpp
+*
+* Summary: The program is to review our coding skills by
+ * creating a class named Textbook, to calculate the amount of books that
+ * needs to be ordered for the students. Also calculates the total profit
+ * made from ordering all the books and selling it.
+*
+*
+*
+* Author: Nelson Nguyen
+* Created: 09/28/23
+* Summary of Modifications:
+ *
+*
+*
+* ***************************************************************/
 
+
+// includes .h file to define the functions
 #include "Textbook.h"
 
 
 
-
+// default constructor
 Textbook::Textbook() {
+    bookcode = 0;
+    onhand = 0;
+    enrollment = 0;
 
+    is_optional = false;
+    is_used = false;
+
+    copyprice = 0;
 };
 
 // mutators
@@ -36,17 +59,26 @@ double Textbook::SetCopyPrice(double copyprice) {
     this->copyprice = copyprice;
 }
 
-int Textbook::NeedToOrder() {
-    int order = 0;
 
+// defined needtoorder function to calculate how many books need
+// to be ordered to determine the profit price.
+int Textbook::NeedToOrder() {
+    int order = 0; // local variable
+
+
+    // if its used then 65% of the class will buy it
+    // calculating amount of books ordered
     if(GetIsUsed()) {
         order = (GetEnrollment() * 0.65) - GetOnHand();
     }
 
-    else if (!GetIsUsed()) {
+    // if it's not used, meaning brand new, 90% of the class will
+    // buy the new book and calculates the amount of books ordered.
+    if (!GetIsUsed()) {
         order = (GetEnrollment() * 0.90) - GetOnHand();
     }
 
+    // returns amount of books need to be ordered.
     return order;
 }
 
